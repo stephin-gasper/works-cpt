885fee0fd7012b2ae6349f00a8a92db848be24f9
Stephin Gasper <stephin.gasper21@gmail.com>
2024-01-06 18:36:52 +0530

    feat: Initial plugin files

---
+++ works-cpt.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Works CPT
+ *
+ * @package       SG_WORKS
+ * @license       mit
+ * @author        Stephin Gasper
+ * @version       1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:   Works CPT
+ * Description:   A CPT for showing projects and contributions made by developer
+ * Version:       1.0.0
+ * Author:        Stephin Gasper
+ * Author URI:    https://stephin-gasper.vercel.app/
+ * Text Domain:   sg_works
+ * License:       MIT
+ * License URI:   https://mit-license.org/
+ */
+
+namespace SG_WORKS;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
